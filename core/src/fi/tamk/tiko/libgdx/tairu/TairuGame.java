d8afd62dd47a39e63853e52dbb72e3e47f0ff0e6
spjass <rautio.juho@gmail.com>
2014-11-23 02:11:56 +0200

    test

--- core/src/fi/tamk/tiko/libgdx/tairu/TairuGame.java
+++ core/src/fi/tamk/tiko/libgdx/tairu/TairuGame.java
@@ -2,14 +2,59 @@ package fi.tamk.tiko.libgdx.tairu;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+
+import java.util.LinkedList;
 
 public class TairuGame extends ApplicationAdapter {
-	SpriteBatch batch;
 	Texture img;
-	
+    private OrthographicCamera camera;
+    TiledMap tiledMap;
+    TiledMapRenderer tiledMapRenderer;
+    SpriteBatch batch;
+    int frame;
+    float width;
+    float height;
+    Music music;
+
+
+    /**
+     * Window width in pixels
+     */
+    private final int WINDOW_WIDTH = 1024;
+
+    /**
+     * Window height in pixels
+     */
+    private final int WINDOW_HEIGHT = 640;
+
+
+    /*
+     * Map size is 42 x 42 tiles.
+     */
+    static int TILES_AMOUNT_WIDTH  = 16;
+    static int TILES_AMOUNT_HEIGHT = 10;
+
+    /**
+     * One tile is 32
+     */
+    static int TILE_WIDTH          = 64;
+    static int TILE_HEIGHT         = 64;
+
+    /**
+     * World in pixels
+     * WORLD_HEIGHT_PIXELS = 10 * 64= 640 pixels
+     * WORLD_WIDTH_PIXELS  = 16 * 64 = 1024 pixels
+     */
+    public static int WORLD_HEIGHT_PIXELS = TILES_AMOUNT_HEIGHT * TILE_HEIGHT;
+    public static int WORLD_WIDTH_PIXELS  = TILES_AMOUNT_WIDTH  * TILE_WIDTH;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
